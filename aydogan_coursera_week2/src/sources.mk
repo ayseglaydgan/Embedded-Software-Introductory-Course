fe9fd309db719b7539091de117757415933761c7
Aysegul Aydogan <a.aydogan2018@gtu.edu.tr>
2021-08-29 03:15:05 +0300

    SOURCES and INCLUDES are added to sources.mk file

--- aydogan_coursera_week2/src/sources.mk
+++ aydogan_coursera_week2/src/sources.mk
@@ -9,9 +9,23 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES =
 
-# Add your include paths to this variable
-INCLUDES = 
+ifeq ($(PLATFORM), MSP432)
+	SOURCES = main.c \
+			  memory.c \
+			  interrupts_msp432p401r_gcc.c \
+			  startup_msp432p401r_gcc.c \
+			  system_msp432p401r.c
+else
+	SOURCES = main.c \
+			  memory.c
+endif
+
+ifeq ($(PLATFORM), MSP432)
+	INCLUDES = -I ../include/common \
+			   -I ../include/CMSIS \
+			   -I ../include/msp432
+else
+	INCLUDES = -I ../include/common
+endif
 
